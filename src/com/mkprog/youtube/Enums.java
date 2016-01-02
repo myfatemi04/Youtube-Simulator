f0207f8a68171564614d5d3d0ef34d9fc59421ec
Psyborg <kevinchung888@yahoo.com>
2016-01-02 10:21:33 -0500

    Steps Class
    Created the steps class which contains the buyCamera and
    buyEditingSoftware functions

--- src/com/mkprog/youtube/Enums.java
+++ src/com/mkprog/youtube/Enums.java
@@ -6,21 +6,31 @@ public class Enums {
 	public enum Camera {
 		camcorder(1,0), phone(3,300), video_camera(4,200), hd_video_camera(6,400), gopro(10,500), movie_camera(15,1000);
 		private double quality;
+		private double cost;
 		private Camera(double quality, double cost) {
 			this.quality = quality;
+			this.cost = cost;
 		}
 		double getQuality() {
 			return quality;
+		}
+		double getCost() {
+			return cost;
 		}
 	}
 	public enum EditingSoftware {
-		movie_maker(3), sony_vegas(3), adobe_premiere(10);
+		raw_video(0.5, 0), movie_maker(3, 15), imovie(4, 50), final_cut(8, 300), adobe_premiere(9, 400), sony_vegas(11, 800);
 		private double quality;
-		private EditingSoftware(double quality) {
+		private double cost;
+		private EditingSoftware(double quality, double cost) {
 			this.quality = quality;
+			this.cost = cost;
 		}
 		double getQuality() {
 			return quality;
+		}
+		double getCost() {
+			return cost;
 		}
 	}
 	public enum VideoType {
